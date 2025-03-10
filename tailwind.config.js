f41ef4b6b94b0f73977bdb8f0474bdefbfd4a992
Anh Minh <nguyenanhminh168202@gmail.com>
2025-03-10 18:09:52 +0700

    Complete Registration UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     screens: {
       sm: "480px",
       md: "768px",
-      lg: "976px",
-      xl: "1420px",
+      lg: "1024px",
+      xl: "1516px",
     },
     colors: {
       textLight: "#D9D9D9",
@@ -30,23 +30,94 @@ module.exports = {
         "linear-gradient(90deg, rgba(217, 217, 217, 0.2) 0%, #D9D9D9 100%)",
       "gradient-tab-dark":
         "linear-gradient(90deg, rgba(41, 41, 41, 0.2) 0%, #292929 100%)",
+
+      radialGradientCircle:
+        "radial-gradient(50% 50% at 50% 50%, rgba(227, 6, 19, 0.1) 0%, rgba(0, 159, 227, 0.2) 100%)",
     },
 
     backgroundPosition: {
       "banner-position": "center",
     },
     fontSize: {
-      "3xl": [
+      "9xl": [
         "7.5rem",
         {
           fontWeight: 700,
-          lineHeight: "100%",
+          lineHeight: "8.75rem",
           letterSpacing: "0",
+        },
+      ],
+      "7xl": [
+        "4.5rem",
+        {
+          fontWeight: 700,
+          lineHeight: "5.75rem",
+          letterSpacing: "0",
+        },
+      ],
+      "6xl": [
+        "3.75rem",
+        {
+          fontWeight: 700,
+          letterSpacing: "0",
+        },
+      ],
+      "5xl": [
+        "3rem",
+        {
+          fontWeight: 700,
+          letterSpacing: "0",
+        },
+      ],
+      "4xl": [
+        "2rem",
+        {
+          fontWeight: 700,
+          letterSpacing: "0",
+        },
+      ],
+      "3xl": [
+        "1.75rem",
+        {
+          fontWeight: 700,
+          letterSpacing: 0,
+        },
+      ],
+      "2xl": [
+        "1.5rem",
+        {
+          fontWeight: 700,
+          letterSpacing: 0,
+        },
+      ],
+      xl: [
+        "1.25rem",
+        {
+          fontWeight: 700,
+          letterSpacing: 0,
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          fontWeight: 400,
+          letterSpacing: 0,
+          lineHeight: "1.5rem",
+        },
+      ],
+      sm: [
+        "0.875rem",
+        {
+          fontWeight: 400,
+          letterSpacing: 0,
+          lineHeight: "1.25rem",
         },
       ],
     },
     fontFamily: {
-      "BigShouldersStencilText-Bold": "BigShouldersStencilText-Bold",
+      BigShouldersStencilText: "BigShouldersStencilText",
+      SpaceGrotesk: "SpaceGrotesk",
+      BigShouldersText: "BigShouldersText",
     },
     extend: {},
   },
